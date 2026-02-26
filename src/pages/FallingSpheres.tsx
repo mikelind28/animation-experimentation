aa962aae691ec43460b1485872135e4fa14935c7
Mike Lind <178736901+mikelind28@users.noreply.github.com>
2026-02-25 23:46:50 -0600

    complete options window for falling spheres

--- src/pages/FallingSpheres.tsx
+++ src/pages/FallingSpheres.tsx
@@ -1,6 +1,12 @@
-import { createTheme, Slider, Stack, ThemeProvider } from "@mui/material";
-import { motion } from "motion/react";
+import { createTheme, Slider, ThemeProvider, ToggleButton, ToggleButtonGroup } from "@mui/material";
+import { styled } from "@mui/material/styles";
+import { AnimatePresence, motion } from "motion/react";
 import { useState } from "react";
+import { FaMoon } from 'react-icons/fa';
+import { FiSun } from "react-icons/fi";
+import { CiCircleInfo } from "react-icons/ci";
+import { IoCloseOutline } from "react-icons/io5";
+
 
 type SphereType = {
     index?: number;
@@ -68,24 +74,63 @@ function Sphere({ radius, xCoor, opacity, delay, duration, hue }: SphereType) {
     )
 }
 
+const CustomSlider = styled(Slider)({
+    color: 'rgba(120, 120, 120, 0.5)',
+    height: 12,
+    width: '95%',
+    '& .MuiSlider-thumb': {
+        height: 24,
+        width: 24,
+        backgroundColor: 'white',
+        border: '1px solid black',
+        '&:focus, &:hover, &.Mui-active, &.Mui-focusVisible': {
+            boxShadow: 'inherit',
+        },
+        '&::before': {
+            display: 'none',
+        },
+    },
+})
+
 const theme = createTheme({
   components: {
-    // Name of the component
     MuiSlider: {
       styleOverrides: {
-        // Name of the slot
         root: {
-          // Some CSS
-          background: 'linear-gradient(90deg in hsl longer hue, red, red)'
+            background: 'linear-gradient(90deg in hsl longer hue, red, red)',
+            padding: '2px',
+            width: '94%',
         },
       },
     },
   },
 });
 
+const RainbowSlider = styled(Slider)({
+    color: 'rgba(0, 0, 0, 0.0)',
+    height: 6,
+    '& .MuiSlider-rail': {
+        opacity: 0
+    },
+    '& .MuiSlider-thumb': {
+        height: 24,
+        width: 24,
+        backgroundColor: 'white',
+        border: '1px solid black',
+        '&:focus, &:hover, &.Mui-active, &.Mui-focusVisible': {
+            boxShadow: 'inherit',
+        },
+        '&::before': {
+            display: 'none',
+        },
+    },
+})
+
 export default function FallingSpheres() {
     const [numberOfSpheres, setNumberOfSpheres] = useState(20);
     const [hue, setHue] = useState(200);
+    const [darkModeOn, setDarkModeOn] = useState(false);
+    const [optionsOpen, setOptionsOpen] = useState(false);
     
     function getRandomRadius() {
         const minRadius = 20;
@@ -116,30 +161,96 @@ export default function FallingSpheres() {
     return (
         <main
             className="h-dvh w-dvw"
+            style={{ backgroundColor: darkModeOn ? 'black' : 'white' }}
         >
-            <Stack spacing={2} direction="row" sx={{ alignItems: 'center', mb: 1 }}>
-                <p>Fewer Spheres</p>
-                <Slider 
-                    aria-label="sphere count" 
-                    step={1}
-                    min={5}
-                    max={35}
-                    value={numberOfSpheres} 
-                    onChange={(_, value) => setNumberOfSpheres(value)} 
-                />
-                <p>More Spheres</p>
-            </Stack>
+            <motion.div 
+                initial={{ width: '72px', height: '72px', borderRadius: '100% 100% 100% 100%' }}
+                animate={
+                    optionsOpen 
+                    ? { width: '320px', height: '250px', borderRadius: '8px 8px 8px 8px'} 
+                    : { width: '72px', height: '72px', borderRadius: '100% 100% 100% 100%' }
+                }
+                transition={{ duration: 0.4 }}
+                className={`
+                    fixed bottom-4 right-4 bg-white drop-shadow-md/25
+                    ${optionsOpen ? 'p-4' : 'flex items-center justify-center'} 
+                `}
+            >
+                <AnimatePresence >
+                    { optionsOpen
+                        ?
+                            <motion.div
+                                key="options"
+                                initial={{ opacity: 0 }}
+                                animate={{ opacity: 1, transition: { duration: 1.5 } }}
+                                exit={{ opacity: 0, transition: { duration: 0 }}}
+                            >
+                                <p>Number of spheres</p>
+                                <CustomSlider 
+                                    aria-label="sphere count" 
+                                    step={1}
+                                    min={5}
+                                    max={35}
+                                    value={numberOfSpheres} 
+                                    onChange={(_, value) => setNumberOfSpheres(value as number)} 
+                                />
 
-            <ThemeProvider theme={theme}>
-                <Slider 
-                    aria-label="sphere hue" 
-                    step={1}
-                    min={0}
-                    max={360}
-                    value={hue} 
-                    onChange={(_, value) => setHue(value)} 
-                />
-            </ThemeProvider>
+                                <p>Color</p>
+                                <ThemeProvider theme={theme}>
+                                    <RainbowSlider 
+                                        aria-label="sphere hue" 
+                                        step={1}
+                                        min={0}
+                                        max={360}
+                                        value={hue} 
+                                        onChange={(_, value) => setHue(value as number)} 
+                                    />
+                                </ThemeProvider>
+
+                                <div className="flex flex-col gap-2 mt-4">
+                                    <p>Dark / Light background</p>
+                                    <ToggleButtonGroup size="large" className="">
+                                        <ToggleButton 
+                                            value={'darkMode'}
+                                            onClick={() => setDarkModeOn(false)}
+                                            selected={!darkModeOn}
+                                            aria-label="light-mode"
+                                        >
+                                            <FiSun className={'text-black'} />
+                                        </ToggleButton>
+
+                                        <ToggleButton 
+                                            value={'lightMode'}
+                                            onClick={() => setDarkModeOn(true)}
+                                            selected={darkModeOn}
+                                            aria-label="dark-mode"
+                                        >
+                                            <FaMoon className={'text-black'} />
+                                        </ToggleButton>
+                                    </ToggleButtonGroup>
+                                </div>
+
+                                <button onClick={() => setOptionsOpen(false)}>
+                                    <IoCloseOutline
+                                        className="absolute right-2 bottom-1 size-12 text-neutral-700"
+                                    />
+                                </button>
+                            </motion.div>
+                        :
+                            <motion.button
+                                key="info"
+                                onClick={() => setOptionsOpen(true)}
+                                animate={{ opacity: 1 }}
+                                exit={{ opacity: 0 }}
+                                transition={{ duration: 0.2 }}
+                            >
+                                <CiCircleInfo 
+                                    className="size-12 text-neutral-600" 
+                                />
+                            </motion.button>
+                    }
+                </AnimatePresence>
+            </motion.div>
 
             <svg
                 xmlns="http://www.w3.org/2000/svg"
