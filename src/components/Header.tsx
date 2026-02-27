e415d4d2cd0b62510d352e6e443a0f10d57b29d7
Mike Lind <178736901+mikelind28@users.noreply.github.com>
2026-02-27 15:09:42 -0600

    add page names and home link to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,41 @@
+import { Link, useLocation } from "react-router";
+import { IoHomeOutline } from "react-icons/io5";
+
 export default function Header() {
+    const location = useLocation();
+    console.log('location', location);
+
+    let currentPage = '';
+
+    switch (location.pathname) {
+        case '/falling-spheres':
+            currentPage = 'Falling Spheres';
+            break;
+        case '/my-proficiencies':
+            currentPage = 'My Proficiencies';
+            break;
+        case '/nature-slideshow':
+            currentPage = 'Nature Slideshow';
+            break;
+        default:
+            break;
+    }
+    
     return (
-        <header>
-            <h1>Animation Experimentation</h1>
+        <header className="sticky top-0 bg-white w-full flex items-center gap-4 p-2">
+            <Link to='/' className="ml-2 p-2 text-2xl border border-black rounded-md">
+                <IoHomeOutline />
+            </Link>
+
+            <div>
+                <h1 className="text-xl text-center">
+                    Animation Experimentation
+                </h1>
+
+                { currentPage && 
+                    <h2>{currentPage}</h2>
+                }
+            </div>
         </header>
     )
 }
